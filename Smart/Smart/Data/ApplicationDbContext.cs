a922330b04ad83e1e10207672a5bedd08ef75eea
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-10-28 19:03:14 -0600

    updated db context

--- Smart/Smart/Data/ApplicationDbContext.cs
+++ Smart/Smart/Data/ApplicationDbContext.cs
@@ -13,11 +13,39 @@ namespace Smart.Data
             : base(options)
         {
         }
+
         public DbSet<Student> Student { get; set; }
-        public DbSet<Assessment> Assessment { get; set; }
-        public DbSet<Class> Class { get; set; }
-        public DbSet<Term> Term { get; set; }
-        public DbSet<Attendance> Attendance { get; set; }
+        public DbSet<ApplicantRating> ApplicantRating { get; set; }
+        public DbSet<RatingCriteria> RatingCriteria { get; set; }
+        public DbSet<File> File { get; set; }
+        public DbSet<FileType> FileType { get; set; }
+        public DbSet<StudentStatus> StudentStatus { get; set; }
         public DbSet<Note> Note { get; set; }
+        public DbSet<NoteType> NoteType { get; set; }
+        public DbSet<StudentClass> StudentClass { get; set; }
+        public DbSet<Term> Term { get; set; }
+        public DbSet<Class> Class { get; set; }
+        public DbSet<ClassInstructor> ClassInstructor { get; set; }
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Attendance> Attendance { get; set; }
+        public DbSet<AttendanceStatus> AttendanceStatus { get; set; }
+        public DbSet<Assessment> Assessment { get; set; }
+        public DbSet<StudentAssessment> StudentAssessment { get; set; }
+        public DbSet<ClassSchedule> ClassSchedule { get; set; }
+        public DbSet<Schedule> Schedule { get; set; }
+        public DbSet<PublicSchoolClassSchedule> PublicSchoolClassSchedule { get; set; }
+        public DbSet<StudentPublicSchoolClass> StudentPublicSchoolClass { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentClass>()
+                .HasKey(c => new { c.StudentId, c.ClassId });
+            modelBuilder.Entity<StudentAssessment>()
+                .HasKey(c => new { c.AssessmentId, c.StudentId });
+            modelBuilder.Entity<ClassSchedule>()
+                .HasKey(c => new { c.ClassId, c.ScheduleId });
+            modelBuilder.Entity<ClassInstructor>()
+                .HasKey(c => new { c.ClassId, c.UserId });
+        }
     }
 }
