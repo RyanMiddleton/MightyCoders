c75b49ca18f51d3f378513626772cb2f651d7534
Dalton Larsen <daltonlarsen@mail.weber.edu>
2019-11-04 07:53:18 -0700

    Added Student Functionality

---
+++ Smart/Smart/Pages/Students/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Smart.Data;
+using Smart.Models;
+
+namespace Smart.Pages.Students
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Smart.Data.ApplicationDbContext _context;
+
+        public DeleteModel(Smart.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Student Student { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student
+                .Include(s => s.StudentStatus).FirstOrDefaultAsync(m => m.StudentId == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Student = await _context.Student.FindAsync(id);
+
+            if (Student != null)
+            {
+                _context.Student.Remove(Student);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
