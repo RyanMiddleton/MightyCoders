a3e20a3c8141cd351127d2822de53732cb768b6d
Ryan Middleton <middletonry@gmail.com>
2019-11-10 23:46:06 -0700

    Changes made to User Management Razor Page and _Layout View

--- Smart/Smart/Models/ScheduleAvailability.cs
+++ Smart/Smart/Models/ScheduleAvailability.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Smart.Models
 {
     public class ScheduleAvailability
     {
+        [Key]
         public int ScheduleAvailabilityId { get; set; }
         public int DayOfWeek { get; set; }
         public DateTime StartTime { get; set; }
