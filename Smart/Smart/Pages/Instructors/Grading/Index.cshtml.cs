f6d3f2ee35e2e9d41c8aa6cbfd0adf6f69a98f66
Peter Tran <Petertran1@weber.edu>
2019-11-12 20:03:42 -0700

    Got Json to post to server and deserialize the json into objects.

--- Smart/Smart/Pages/Instructors/Grading/Index.cshtml.cs
+++ Smart/Smart/Pages/Instructors/Grading/Index.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using Smart.Data;
 using Smart.Models;
 using Smart.Models.InstructorViewModels;
@@ -14,18 +17,29 @@ namespace Smart.Pages.Instructors.Grading
 {
     public class IndexModel : PageModel
     {
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Age { get; set; }
+        }
         private readonly Smart.Data.ApplicationDbContext _context;
-
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    services.AddMvc();
+        //    services.AddAntiforgery(o => o.HeaderName = "RequestVerificationToken");
+        //}
         public IndexModel(Smart.Data.ApplicationDbContext context)
         {
             _context = context;
         }
         [BindProperty]
         public int clsId { get; set; }
+        [BindProperty]
         public GradingIndexData GradingData { get; set; }
         [BindProperty]
         public List<SelectListItem> Classes { get; set; }
-        public async Task<IActionResult> OnGetAsync(int? classId)
+        public async Task<IActionResult> OnGetAsync(int? classId, List<StudentAssessment> studentAssessments)
         {
             GradingData = new GradingIndexData();
             GradingData.Classes = await _context.Class.ToListAsync();
@@ -39,9 +53,6 @@ namespace Smart.Pages.Instructors.Grading
                                                 Value = c.ClassId.ToString(),
                                                 Text = c.Course.Name.ToString()
                                             }).ToListAsync();
-            //.Include(a => a.Assessments)
-            //.Include(a => a.Students)
-            //.Include(a => a.StudentAssessments).ToListAsync();
 
             if (classId != null)
             {
@@ -60,5 +71,93 @@ namespace Smart.Pages.Instructors.Grading
 
             return Page();
         }
+
+
+        public JsonResult OnPost(List<StudentAssessment> assessments)
+        {
+            //List<StudentAssessment> sa = new List<StudentAssessment>();
+            //foreach(StudentAssessment studentAssessment in studentAssessments)
+            //{
+            //sa.Add(studentAssessment);
+            //}
+            int sPostValue1;
+            double sPostValue2;
+            string sPostValue3 = "";
+            {
+                MemoryStream stream = new MemoryStream();
+                Request.Body.CopyTo(stream);
+                stream.Position = 0;
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string requestBody = reader.ReadToEnd();
+                    if (requestBody.Length > 0)
+                    {
+                        dynamic dyn = JsonConvert.DeserializeObject(requestBody);
+                        var lststudass = new List<StudentAssessment>();
+                        foreach(var obj in dyn)
+                        {
+                            lststudass.Add(new StudentAssessment()
+                            {
+                                StudentId = obj.StudentId,
+                                PointsAwarded = obj.PointsAwarded,
+                                Comments = obj.Comments
+                            }
+                            );
+                        }
+                        //if (obj != null)
+                        //{
+                        //    sPostValue1 = obj.StudentId;
+                        //    //sPostValue2 = obj.PointsAwarded;
+                        //    //sPostValue3 = obj.Comments;
+                        //}
+                    }
+                }
+            }
+            return new JsonResult("test");
+                return new JsonResult("test: test");
+            //do something with the person class
+        }
+
+        public class StudentAssessmentTemp
+        {
+
+            //public int AssessmentId { get; set; }
+            //public virtual Assessment Assessment { get; set; }
+
+            public int StudentId { get; set; }
+            //public virtual Student Student { get; set; }
+            public double PointsAwarded { get; set; }
+            public string Comments { get; set; }
+        }
+
+        //public ActionResult OnPostSend()
+        //{
+        //    List<string> lstString = new List<string>
+        //    {
+        //        "test",
+        //        "test2",
+        //        "test3"
+        //    };
+        //    return new JsonResult(lstString);
+        //    //using (CustomersEntities entities = new CustomersEntities())
+        //    //{
+        //    //    //Truncate Table to delete all old records.
+        //    //    entities.Database.ExecuteSqlCommand("TRUNCATE TABLE [Customers]");
+
+        //    //    //Check for NULL.
+        //    //    if (customers == null)
+        //    //    {
+        //    //        customers = new List<Customer>();
+        //    //    }
+
+        //    //    //Loop and insert records.
+        //    //    foreach (Customer customer in customers)
+        //    //    {
+        //    //        entities.Customers.Add(customer);
+        //    //    }
+        //    //    int insertedRecords = entities.SaveChanges();
+        //    //    return Json(insertedRecords);
+        //    //}
+        //}
     }
 }
