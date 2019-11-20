7092ebbd132100304cb415bd73f234e1c260b291
Ryan Middleton <middletonry@gmail.com>
2019-11-20 09:02:39 -0700

    Commiting Authorization functionality

--- Smart/Smart/Pages/Students/Create.cshtml.cs
+++ Smart/Smart/Pages/Students/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -10,6 +11,7 @@ using Smart.Models;
 
 namespace Smart.Pages.Students
 {
+    [Authorize(Roles = "Instructor, Admin")]
     public class CreateModel : StatusNameModel
     {
         private readonly Smart.Data.ApplicationDbContext _context;
