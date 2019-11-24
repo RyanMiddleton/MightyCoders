b827d1c36f1f28f28356f7ec14bc6cd3ddeba24d
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-11-23 20:39:24 -0700

    added creating new schedule availability

--- Smart/Smart/Pages/ScheduleAvailabilities/Index.cshtml.cs
+++ Smart/Smart/Pages/ScheduleAvailabilities/Index.cshtml.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Smart.Data;
+using Smart.Utility;
 using Smart.Models;
 
 namespace Smart.Pages.ScheduleAvailabilities
@@ -21,6 +23,9 @@ namespace Smart.Pages.ScheduleAvailabilities
 
         [BindProperty]
         public List<ScheduleAvailability> ScheduleAvailabilities { get; set; }
+        [BindProperty]
+        public ScheduleAvailability ScheduleAvailability { get; set; }
+        public List<SelectListItem> DaysOfWeek { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -29,6 +34,20 @@ namespace Smart.Pages.ScheduleAvailabilities
                                               .OrderBy(s => s.DayOfWeek)
                                               .ToListAsync();
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var scheduleToAdd = await _db.ScheduleAvailability.SingleOrDefaultAsync(sa => sa.DayOfWeek == ScheduleAvailability.DayOfWeek && sa.StartTime == ScheduleAvailability.StartTime && sa.EndTime == ScheduleAvailability.EndTime);
+                if (scheduleToAdd == null)
+                {
+                    _db.ScheduleAvailability.Add(ScheduleAvailability);
+                    await _db.SaveChangesAsync();
+                }
+            }
+            return await OnGetAsync();
         }
 
         public async Task<IActionResult> OnGetRemoveAsync(int? scheduleId)
