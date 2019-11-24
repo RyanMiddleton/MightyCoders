983639a264f272fd1f83c6d5718f49f3fce2a2e7
jakefowler <46506569+jakefowler@users.noreply.github.com>
2019-11-23 18:46:12 -0700

    added schedule availabilities page and displayed the availabilities

---
+++ Smart/Smart/Pages/ScheduleAvailabilities/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Smart.Data;
+using Smart.Models;
+
+namespace Smart.Pages.ScheduleAvailabilities
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public List<ScheduleAvailability> ScheduleAvailabilities { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ScheduleAvailabilities = await _db.ScheduleAvailability
+                                              .OrderBy(t => t.StartTime)
+                                              .OrderBy(s => s.DayOfWeek)
+                                              .ToListAsync();
+            return Page();
+
+        }
+    }
+}
